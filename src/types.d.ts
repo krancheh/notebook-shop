16ed4b5d0b0391875abc28da98c3f10a08159152
Kirill <krancheh@gmail.com>
2024-03-13 23:36:11 +0300

    add auth data types

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,3 +3,12 @@
 export interface Params {
     [key: string]: string | number;
 }
+
+export interface LoginUserData {
+    email: string;
+    password: string;
+}
+export interface SignupUserData extends LoginUserData {
+    firstName: string;
+    lastName: string;
+} 
