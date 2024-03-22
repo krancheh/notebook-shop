0327a6e9e43351ce389a098e8c6395b096fce9a4
krancheh <krancheh@gmail.com>
2024-03-22 19:49:04 +0300

    add formatting price function

---
+++ src/utils/getFormattedPrice.ts
@@ -0,0 +1,13 @@
+
+
+
+const getFormattedPrice = (price: number) => {
+    let formattedPrice = price.toString();
+    if (formattedPrice.length > 3) {
+        // for(let i = formattedPrice.length / 3)
+    }
+
+    return formattedPrice;
+}
+
+export default getFormattedPrice;
